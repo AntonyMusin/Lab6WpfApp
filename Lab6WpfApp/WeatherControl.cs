ccf43ebf18802e6131c11238a80e0ea4542085e7
Professional <Professional@USER-PC>
2021-11-30 00:55:18 +0300

    Добавьте файлы проекта.

---
+++ Lab6WpfApp/WeatherControl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab6WpfApp
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty;
+        private string windDirection { get; set; }
+        private string windSpeed { get; set; }
+        private enum Precipitation //Не уверен, что правильно enum для свойства "Осадки" прописал
+        {
+            Sunny,
+            Cloudy,
+            Rainy,
+            Snowy
+        }
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+        static WeatherControl()   //Екатерина, если я верно помню, мы можем для класса один конструктор назначать?
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)),
+                    new ValidateValueCallback(ValidateTemperature));
+        }
+
+        private static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Температура:{Temperature} Направление ветра: {windDirection} Скорость ветра: {windSpeed} Осадки: {Enum.GetNames(typeof(Precipitation))}");
+        }
+    }
+}
